0b71afa813ac325e065d6128e65dd42094a2fea5
gcastrocastro <gcastrocastro.25@gmail.com>
2023-05-05 14:27:43 -0400

    add router, relocate components and pages, add pages

--- src/index.js
+++ src/index.js
@@ -1,15 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import App from './components/App/App';
 import reportWebVitals from './reportWebVitals';
+import {BrowserRouter} from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
+
+//HOC Pattern: Higher Order Component Pattern-- think High Order Function, but with components
+// High Order Function-- function that takes a function as an argument to be called at a later time
+//High Order Component Pattern-- component that takes in another component to be called at a later time.
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
---
+++ src/pages/Starship/Starship.js
@@ -0,0 +1,5 @@
+function Starship(props) {
+    return <h1>This is the Starship Component </h1>
+}
+
+export default Starship;
